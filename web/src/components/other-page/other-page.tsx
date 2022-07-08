0e2c3eb314b50d4c0e16ce598e9d2bfceae7538f
z3zzz <zabra7946@naver.com>
2022-07-08 11:25:51 +0900

    Update web: for k8s testing purpose

--- web/src/components/other-page/other-page.tsx
+++ web/src/components/other-page/other-page.tsx
@@ -5,7 +5,7 @@ const OtherPage: React.FC = () => {
   return (
     <div className="other-columns">
       <div className="other-column-link">
-        <Link to="/">Go back to home</Link>
+        <Link to="/">Go back to home!!!!</Link>
       </div>
       <div className="other-column-main">
         <h1>Other Page</h1>
